4d8f316e13dcbd78485778934d07aabf3080cec1
jsrobson10 <45777442+jsrobson10@users.noreply.github.com>
2019-02-02 18:44:30 +1100

    The original mainloop library files

---
+++ mainloop.h
@@ -0,0 +1,7 @@
+void mainloopInit();
+void mainloopDoTick();
+void mainloopDoMainloop();
+void mainloopOnEarly(void reg());
+void mainloopOnLate(void reg());
+void mainloopRegAction(void reg(int*), unsigned long millis, int *args);
+void mainloopStop();
